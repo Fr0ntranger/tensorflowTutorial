31464040a31a32224d3c925e680b1668263381f1
Wilson Burchenal <wburchenal@8x8.com>
2018-01-04 16:12:29 -0800

    Basic CNN for MNIST with ADAMOptimizer

--- examples/07_convnet_mnist_starter.py
+++ examples/07_convnet_mnist_starter.py
@@ -29,8 +29,9 @@ LEARNING_RATE = 0.001
 BATCH_SIZE = 128
 SKIP_STEP = 10
 DROPOUT = 0.75
-N_EPOCHS = 1
+N_EPOCHS = 25
 
+print("FALL, BALMUNG")
 # Step 3: create placeholders for features and labels
 # each image in the MNIST data is of shape 28*28 = 784
 # therefore, each image is represented with a 1x784 tensor
@@ -48,59 +49,42 @@ dropout = tf.placeholder(tf.float32, name='dropout')
 
 global_step = tf.Variable(0, dtype=tf.int32, trainable=False, name='global_step')
 
-utils.make_dir('checkpoints')
-utils.make_dir('checkpoints/convnet_mnist')
+# os.makedirs('checkpoints')
+# os.makedirs('checkpoints/convnet_mnist')
 
 with tf.variable_scope('conv1') as scope:
     # first, reshape the image to [BATCH_SIZE, 28, 28, 1] to make it work with tf.nn.conv2d
     # use the dynamic dimension -1
     images = tf.reshape(X, shape=[-1, 28, 28, 1])
-    
-    # TO DO
-
     # create kernel variable of dimension [5, 5, 1, 32]
     # use tf.truncated_normal_initializer()
-    
-    # TO DO
-
+    kernel = tf.get_variable('kernels', [5, 5, 1, 32], initializer=tf.truncated_normal_initializer())
     # create biases variable of dimension [32]
     # use tf.constant_initializer(0.0)
-    
-    # TO DO 
-
+    # Change initializer to tf.random_normal_init... MAYBE??
+    biases = tf.get_variable('biases', [32], initializer=tf.constant_initializer(0.0))
     # apply tf.nn.conv2d. strides [1, 1, 1, 1], padding is 'SAME'
-    
-    # TO DO
-
+    conv = tf.nn.conv2d(images, kernel, strides=[1,1,1,1], padding='SAME')
     # apply relu on the sum of convolution output and biases
-    
-    # TO DO 
-
+    conv1 = tf.nn.relu(conv+biases, name=scope.name)
     # output is of dimension BATCH_SIZE x 28 x 28 x 32
 
 with tf.variable_scope('pool1') as scope:
-    # apply max pool with ksize [1, 2, 2, 1], and strides [1, 2, 2, 1], padding 'SAME'
-    
-    # TO DO
-
+    # apply max pool with ksize [1, 2, 2, 1], and strides [1, 2, 2, 1], padding 'SAME'    
+    pool1 = tf.nn.max_pool(conv1, ksize=[1,2,2,1], strides=[1,2,2,1], padding='SAME')
     # output is of dimension BATCH_SIZE x 14 x 14 x 32
 
 with tf.variable_scope('conv2') as scope:
     # similar to conv1, except kernel now is of the size 5 x 5 x 32 x 64
-    kernel = tf.get_variable('kernels', [5, 5, 32, 64], 
-                        initializer=tf.truncated_normal_initializer())
-    biases = tf.get_variable('biases', [64],
-                        initializer=tf.random_normal_initializer())
+    kernel = tf.get_variable('kernels', [5, 5, 32, 64], initializer=tf.truncated_normal_initializer())
+    biases = tf.get_variable('biases', [64], initializer=tf.random_normal_initializer())
     conv = tf.nn.conv2d(pool1, kernel, strides=[1, 1, 1, 1], padding='SAME')
     conv2 = tf.nn.relu(conv + biases, name=scope.name)
-
     # output is of dimension BATCH_SIZE x 14 x 14 x 64
 
 with tf.variable_scope('pool2') as scope:
     # similar to pool1
-    pool2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1],
-                            padding='SAME')
-
+    pool2 = tf.nn.max_pool(conv2, ksize=[1, 2, 2, 1], strides=[1, 2, 2, 1], padding='SAME')
     # output is of dimension BATCH_SIZE x 7 x 7 x 64
 
 with tf.variable_scope('fc') as scope:
@@ -108,8 +92,8 @@ with tf.variable_scope('fc') as scope:
     input_features = 7 * 7 * 64
     
     # create weights and biases
-
-    # TO DO
+    w = tf.get_variable('weights', [input_features, 1024], initializer=tf.truncated_normal_initializer())
+    b = tf.get_variable('biases', [1024], initializer=tf.constant_initializer(0.0))
 
     # reshape pool2 to 2 dimensional
     pool2 = tf.reshape(pool2, [-1, input_features])
@@ -117,30 +101,29 @@ with tf.variable_scope('fc') as scope:
     # apply relu on matmul of pool2 and w + b
     fc = tf.nn.relu(tf.matmul(pool2, w) + b, name='relu')
     
-    # TO DO
-
     # apply dropout
     fc = tf.nn.dropout(fc, dropout, name='relu_dropout')
 
 with tf.variable_scope('softmax_linear') as scope:
     # this you should know. get logits without softmax
     # you need to create weights and biases
-
-    # TO DO
+    w = tf.get_variable('weights', [1024, N_CLASSES], initializer=tf.truncated_normal_initializer())
+    b = tf.get_variable('biases', [N_CLASSES], initializer=tf.random_normal_initializer())
+    logits = tf.matmul(fc,w) + b
 
 # Step 6: define loss function
 # use softmax cross entropy with logits as the loss function
 # compute mean cross entropy, softmax is applied internally
 with tf.name_scope('loss'):
     # you should know how to do this too
-    
-    # TO DO
+    entropy = tf.nn.softmax_cross_entropy_with_logits(labels=Y, logits=logits)
+    loss = tf.reduce_mean(entropy, name='loss')
 
 # Step 7: define training op
 # using gradient descent with learning rate of LEARNING_RATE to minimize cost
 # don't forgot to pass in global_step
 
-# TO DO
+optimizer = tf.train.AdamOptimizer(LEARNING_RATE).minimize(loss,global_step=global_step)
 
 with tf.Session() as sess:
     sess.run(tf.global_variables_initializer())
